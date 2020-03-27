50adb243c4bfccd788808355fa2ba012b5bb0807
PhyoHtetAung <phyohtetaung.dev.29@gmail.com>
2020-03-27 17:07:03 +0630

    finishing sign in handleSubmit method

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -3,7 +3,7 @@ import './sign-in.styles.scss';
 
 import FormInput from './../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
-import { signInWithGoogle } from './../../firebase/firebase.utils';
+import { auth, signInWithGoogle } from './../../firebase/firebase.utils';
 
 class SignIn extends React.Component {
     constructor(props) {
@@ -15,10 +15,15 @@ class SignIn extends React.Component {
         }
     }
 
-    handleSubmit = event => {
-
-        console.log(this.state.email);
-        this.setState({ email: '', password: '' });
+    handleSubmit = async event => {
+        event.preventDefault();
+        const { email, password } = this.state;
+        try {
+            await auth.signInWithEmailAndPassword(email, password);
+            this.setState({ email: '', password: '' });
+        } catch (error) {
+            console.error(error);
+        }
     }
 
     handleChange = event => {
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -16,9 +16,7 @@ const config = {
 
 export const createUserProfileDocument = async (userAuth, additionalData) => {
     if (!userAuth) return;
-    console.log(userAuth);
     const userRef = firestore.doc(`users/${userAuth.uid}`);
-    console.log(userRef);
 
     const snapShot = await userRef.get();
 
